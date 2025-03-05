6291785925932ff5cec8f8b29a5254db3ea4a781
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-03-05 16:16:23 +0300

    Initialize Expo app with basic structure and configuration

---
+++ constants/colors.ts
@@ -0,0 +1,17 @@
+export const Colors = {
+  light: {
+    primary: "#0961F5",
+    background: "#F5F9FF",
+    tint: "#E8F1FF",
+    secondary: "#167F71",
+    accent: "#FF6B00",
+    titles: "#202244",
+    subtitles: "#545454",
+    muted: "#A0A4AB",
+  },
+  dark: {
+    background: "#000",
+    titles: "#FFF",
+    subtitles: "#A0A4AB",
+  },
+};
