c80c19f0e89b8053f850bb096ced1a364e3fc9bf
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-03-05 17:29:34 +0300

    Implement onboarding screen and update layout with font loading and navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,45 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Stack, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const router = useRouter();
+  const [loaded] = useFonts({
+    WorkSansRegular: require("../assets/fonts/WorkSans-Regular.ttf"),
+    WorkSansBold: require("../assets/fonts/WorkSans-Bold.ttf"),
+    WorkSansSemibold: require("../assets/fonts/WorkSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="onboarding"
+        options={{
+          title: "Onboarding",
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,12 @@
-import { Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "@/constants/colors";
+
+const { width, height } = Dimensions.get("window");
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View
       style={{
@@ -9,7 +15,66 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require("../assets/images/index.png")}
+        style={{
+          objectFit: "contain",
+          height: width * 0.9,
+          width: width,
+        }}
+      />
+      <Text
+        style={{
+          fontSize: 34,
+          fontFamily: "WorkSansSemibold",
+          width: width * 0.7,
+        }}
+      >
+        Welcome to
+      </Text>
+      <Text
+        style={{
+          fontSize: 34,
+          fontFamily: "WorkSansSemibold",
+          width: width * 0.7,
+          marginTop: 5,
+        }}
+      >
+        Block Academy
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontFamily: "WorkSansRegular",
+          width: width * 0.7,
+          marginTop: 20,
+        }}
+      >
+        Experience the future of education with smart tutoring, interactive
+        challenges, and decentralized rewards.
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: Colors.light.primary,
+          padding: 10,
+          paddingVertical: 15,
+          width: width * 0.7,
+          borderRadius: 10,
+          marginTop: 40,
+          alignItems: "center",
+        }}
+        onPress={() => router.push("/onboarding")}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            fontFamily: "WorkSansSemibold",
+          }}
+        >
+          Get Started
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
